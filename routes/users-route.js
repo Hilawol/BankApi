e0a5f9e155dbae8ecbdab995dbcb5e1eaf52d40c
Hila Kershenovich <hilawol@gmail.com>
2021-04-15 02:07:41 +0300

    added deposit route

--- routes/users-route.js
+++ routes/users-route.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const usersController = require('../controllers/users-controller');
 
-const users = require('../data/users.json');
+router.get('/', usersController.getAllUsers)
+  .get('/:id', usersController.getUserById)
+  .post('/', (req, res) => {
 
-router.get('/', (req, res) => {
-  return res.status(200).send(users);
-
-}).get('/:id', (req, res) => {
-  const { id } = req.params;
-  const user = users.find(u => u.id == id);
-  if (user) {
-    return res.status(200).send(user);
-  }
-  else {
-    return res.status(404).send("User not found.");
-  }
-}).post('/', (req, res) => {
-
-});
+  })
+  .put('/:id/deposit', usersController.deposit);
 
 module.exports = router;
