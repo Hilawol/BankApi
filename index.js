f3cb36292fd51034deacaff1a8f8fd6bc42b097d
Hila Kershenovich <hilawol@gmail.com>
2021-04-18 14:59:05 +0300

    changed port

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
-const port = 8000;
+const port = 5000;
 
 const usersRoute = require('./routes/users-route');
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/bank/users', usersRoute);
 
-app.listen(port, () => {
+app.listen(process.nextTick.PORT || port, () => {
   console.log(`application start at ${port}`);
 })
