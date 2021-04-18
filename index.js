946b6e33d512ef32587775e9681c41bfb2e8f4a5
Hila Kershenovich <hilawol@gmail.com>
2021-04-18 15:42:46 +0300

    fixed port

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const usersRoute = require('./routes/users-route');
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.get('/', (req, res) => {
+  res.json({ success: { id: 1, email: 'asfasf@asfasf.com' } })
+})
 app.use('/bank/users', usersRoute);
 
-app.listen(process.nextTick.PORT || port, () => {
-  console.log(`application start at ${port}`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`application start at ${5000}`);
 })
